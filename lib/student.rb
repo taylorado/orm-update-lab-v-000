acef38cb6ac868238c0c89d93ba5665559d42edc
Taylor Donlan <taylordonlan@gmail.com>
2018-07-10 18:14:42 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,14 +52,11 @@ class Student
   end
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
     student = self.new(row[1], row[2], row[0])
     student
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
     sql = <<-SQL
       SELECT * FROM students
       WHERE name = ?
